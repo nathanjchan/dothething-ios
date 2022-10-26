a1082b0122719e1bc1f47367f23ae0fd400dcb3a
Nathan Chan <nathanjchan2@gmail.com>
2022-10-26 00:29:34 -0700

    Working: upload to existing thing!

--- dothething/dothething/ClipsView.swift
+++ dothething/dothething/ClipsView.swift
@@ -67,12 +67,7 @@ struct ClipsView: View {
                 .opacity(clipsViewModel.shareDisabled ? 0.5 : 1)
             }
 
-            if clipsViewModel.isLoading {
-                ProgressView()
-                    .scaleEffect(x: 2, y: 2, anchor: .center)
-                    .padding(.top)
-                Spacer()
-            } else if !clipsViewModel.errorText.isEmpty {
+            if !clipsViewModel.errorText.isEmpty {
                 Text(clipsViewModel.errorText)
                     .padding(.top)
                     .foregroundColor(Color(UIColor.systemGray))
@@ -126,7 +121,6 @@ extension ClipsView {
 //            Clip(url: URL(string: "https://www.youtube.com/watch?v=9bZkp7q19f0") ?? URL(fileURLWithPath: ""), thumbnail: UIImage(systemName: "film") ?? UIImage(), isHighlighted: false),
 //            Clip(url: URL(string: "https://www.youtube.com/watch?v=p3G5IXn0K7A") ?? URL(fileURLWithPath: ""), thumbnail: UIImage(systemName: "film") ?? UIImage(), isHighlighted: false)
         ]
-        @Published var isLoading = false
         @Published var shareDisabled = true
         @Published var uploadDisabled = true
         @Published var errorText = ""
@@ -134,18 +128,22 @@ extension ClipsView {
         private var code = ""
         private var videoDegreesToRotate = -90
         private let password = "ThisIsEpicPassword"
-
         private lazy var imagePicker = ImagePicker(viewModel: self)
 
         init() {
             print("Initializing ClipsViewModel")
+        }
+        
+        private func clearStorage() {
+            DispatchQueue.main.async {
+                self.clips = []
+            }
         }
 
         func enterCode(code: String) {
             if self.clips.isEmpty {
                 print("Entered enterCode with code: \(code)")
                 self.code = code
-                self.isLoading = true
                 self.downloadExistingThing()
             } else {
                 print("Ignoring enterCode because clips is empty")
@@ -188,42 +186,28 @@ extension ClipsView {
         private func stopLoadingDueToError(text: String) {
             DispatchQueue.main.async {
                 self.errorText = text
-                self.isLoading = false
             }
         }
         
-        private func handleStatusCodeError(statusCode: Int) {
-            print("statusCode should be 200, but is \(statusCode)")
-            if statusCode == 404 {
-                self.stopLoadingDueToError(text: "Status Code 404: Invalid Code")
-            } else {
-                self.stopLoadingDueToError(text: "Status Code \(statusCode)")
-            }
-        }
-        
-        private func handleGenericError(errorCode: String) {
+        private func handleError(errorCode: String, logMessage: String = "") {
             self.stopLoadingDueToError(text: "Error Code \(errorCode)")
-        }
-
-        private func clearStorage() {
-            DispatchQueue.main.async {
-                self.clips = []
+            if !logMessage.isEmpty {
+                print(logMessage)
             }
         }
 
         struct ClipMetaData: Codable {
             var code: String
             var id: String
-            var timestamp: String
         }
 
-        // DEFCON 1.1
-        // GET request
-        // must provide "code" header
+        // DEFCON 1
         func downloadExistingThing() {
             print("Entered downloadExistingThing")
             print("Code: \(self.code)")
-//            self.clearStorage()
+            DispatchQueue.main.async {
+                self.uploadDisabled = true
+            }
 
             // GET request to get presigned URL
             let url = URL(string: "https://kenv1ez376.execute-api.us-west-1.amazonaws.com/alpha/dothething") ?? URL(fileURLWithPath: "")
@@ -236,11 +220,11 @@ extension ClipsView {
             let task = URLSession.shared.dataTask(with: request) { data, response, error in
                 // data validation
                 guard let data = data, error == nil else {
-                    print("error=\(String(describing: error))")
+                    self.handleError(errorCode: "D1")
                     return
                 }
                 if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
-                    self.handleStatusCodeError(statusCode: httpStatus.statusCode)
+                    self.handleError(errorCode: "D11-\(httpStatus.statusCode)")
                     return
                 }
                 print("response = \(String(describing: response))")
@@ -250,10 +234,16 @@ extension ClipsView {
                 let decoder = JSONDecoder()
                 guard let dataArray = try? decoder.decode([ClipMetaData].self, from: data) else {
                     print("Failed to decode JSON")
-                    self.handleGenericError(errorCode: "1")
+                    self.handleError(errorCode: "D2")
                     return
                 }
                 print("DownloadResponse: \(dataArray)")
+                
+                // handle empty response
+                if dataArray.isEmpty {
+                    self.handleError(errorCode: "INVALID_CODE")
+                    return
+                }
                 
                 // for each id, make a GET request to get the presigned URL
                 for clip in dataArray {
@@ -267,11 +257,11 @@ extension ClipsView {
                     let task = URLSession.shared.dataTask(with: request) { data, response, error in
                         // data validation
                         guard let data = data, error == nil else {
-                            self.handleGenericError(errorCode: "2")
+                            self.handleError(errorCode: "D3")
                             return
                         }
                         if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
-                            self.handleStatusCodeError(statusCode: httpStatus.statusCode)
+                            self.handleError(errorCode: "D31-\(httpStatus.statusCode)")
                             return
                         }
                         print("response = \(String(describing: response))")
@@ -286,11 +276,11 @@ extension ClipsView {
                         let task = URLSession.shared.dataTask(with: request) { data, response, error in
                             // data validation
                             guard let data = data, error == nil else {
-                                self.handleGenericError(errorCode: "3")
+                                self.handleError(errorCode: "D4")
                                 return
                             }
                             if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
-                                self.handleStatusCodeError(statusCode: httpStatus.statusCode)
+                                self.handleError(errorCode: "D41-\(httpStatus.statusCode)")
                                 return
                             }
 
@@ -303,7 +293,7 @@ extension ClipsView {
                                 print("Saved data to \(tempFileUrl)")
                             } catch {
                                 print("Error saving data to \(tempFileUrl)")
-                                self.handleGenericError(errorCode: "2")
+                                self.handleError(errorCode: "D5")
                                 return
                             }
 
@@ -320,7 +310,6 @@ extension ClipsView {
 
                                 // if all clips have been downloaded, stop loading
                                 if self.clips.count >= dataArray.count {
-                                    self.isLoading = false
                                     self.uploadDisabled = false
                                 }
                             }
@@ -332,54 +321,9 @@ extension ClipsView {
             }
             task.resume()
         }
-
-        // DEFCON 2.2
-        // PUT request
-        // will actually put data
-        // must provide "code" header
-        func uploadVideo(presignedUrl: String, videoUrl: URL) {
-            print("Entered uploadVideo")
-            print("Presigned URL: \(presignedUrl)")
-            
-            // convert NSData to data
-            let videoData = NSData(contentsOf: videoUrl)
-            guard let data = videoData as Data? else {
-                self.handleGenericError(errorCode: "F3")
-                return
-            }
-
-            // alert user if video is too large (1 GB)
-            if data.count > 1000000000 {
-                self.handleGenericError(errorCode: "FILE_TOO_LARGE")
-                return
-            }
-
-            let url = URL(string: presignedUrl)
-            var request = URLRequest(url: url ?? URL(fileURLWithPath: ""))
-            request.httpMethod = "PUT"
-
-            print("Starting PUT request to \(String(describing: url))")
-            let task = URLSession.shared.uploadTask(with: request, from: data) { data, response, error in
-                // data validation
-                guard let data = data, error == nil else {
-                    return
-                }
-                if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
-                    self.handleStatusCodeError(statusCode: httpStatus.statusCode)
-                    return
-                }
-                let responseString = String(data: data, encoding: .utf8)
-                print("response = \(String(describing: response))")
-                print("responseString = \(String(describing: responseString))")
-            }
-            task.resume()
-        }
         
-        // DEFCON 2.1
-        // PUT request
-        // doesn't actually put any data
-        // must provide "code" header
-        func getPresignedUrlForUploadToExistingThing(videoUrl: URL) {
+        // DEFCON 2
+        func uploadToExistingThing(videoUrl: URL) {
             print("Entered getPresignedUrlForUploadToExistingThing")
             print("Code: \(self.code)")
             print("Video URL: \(videoUrl)")
@@ -389,38 +333,80 @@ extension ClipsView {
             print("File extension: \(fileExtension)")
 
             // PUT request to get presigned URL
-            let url = URL(string: "https://kenv1ez376.execute-api.us-west-1.amazonaws.com/alpha/dothething") ?? URL(fileURLWithPath: "")
+            let url = URL(string: "https://kenv1ez376.execute-api.us-west-1.amazonaws.com/alpha/dothething")
+            guard let url = url else {
+                self.handleError(errorCode: "E1")
+                return
+            }
             var request = URLRequest(url: url)
             request.httpMethod = "PUT"
             request.setValue(self.code, forHTTPHeaderField: "code")
+            request.setValue(self.password, forHTTPHeaderField: "password")
             request.setValue(fileExtension, forHTTPHeaderField: "file-extension")
 
             print("Starting PUT request to \(String(describing: url))")
             let task = URLSession.shared.dataTask(with: request) { data, response, error in
                 // data validation
                 guard let data = data, error == nil else {
-                    print("error=\(String(describing: error))")
+                    self.handleError(errorCode: "E2")
                     return
                 }
                 if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
-                    self.handleStatusCodeError(statusCode: httpStatus.statusCode)
+                    self.handleError(errorCode: "E21-\(httpStatus.statusCode)")
                     return
                 }
-                let responseString = String(data: data, encoding: .utf8)
-                print("response = \(String(describing: response))")
-                print("responseString = \(String(describing: responseString))")
+
+                // convert data to string to get presigned URL
+                let presignedUrl = String(data: data, encoding: .utf8)
+                guard let presignedUrl = presignedUrl else {
+                    self.handleError(errorCode: "E3")
+                    return
+                }
+                print("Response: \(String(describing: response))")
+                print("Presigned URL: \(String(describing: presignedUrl))")
                 
-                self.uploadVideo(presignedUrl: responseString ?? "", videoUrl: videoUrl)
+                // convert NSData to data
+                let videoData = NSData(contentsOf: videoUrl)
+                guard let data = videoData as Data? else {
+                    self.handleError(errorCode: "E4")
+                    return
+                }
+
+                // alert user if video is too large (0.5 GB)
+                if data.count > 536870912 {
+                    self.handleError(errorCode: "FILE_TOO_LARGE")
+                    return
+                }
+
+                let url = URL(string: presignedUrl)
+                guard let url = url else {
+                    self.handleError(errorCode: "E5")
+                    return
+                }
+                var request = URLRequest(url: url)
+                request.httpMethod = "PUT"
+
+                print("Starting PUT request to \(String(describing: url))")
+                let task = URLSession.shared.uploadTask(with: request, from: data) { data, response, error in
+                    if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                        self.handleError(errorCode: "E51-\(httpStatus.statusCode)", logMessage: String(describing: response))
+                        return
+                    }
+                    print("response = \(String(describing: response))")
+
+                    // reload everything
+                    sleep(3)
+                    self.clearStorage()
+                    self.downloadExistingThing()
+                }
+                task.resume()
             }
             task.resume()
         }
         
         // DEFCON 2.0
         func upload(videoUrl: URL) {
-            DispatchQueue.main.async {
-                self.isLoading = true
-            }
-            self.getPresignedUrlForUploadToExistingThing(videoUrl: videoUrl)
+            self.uploadToExistingThing(videoUrl: videoUrl)
         }
         
         func getPresignedUrlForUploadToNewThing() {
